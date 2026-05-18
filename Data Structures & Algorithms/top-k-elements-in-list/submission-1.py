cef1bb0e5a8fa0d82b156bc439f847f05a7e36e7
Sanjith Kumar <53137451+thesanjithkumar@users.noreply.github.com>
2026-05-18 20:13:44 +0530

    Add: top-k-elements-in-list - submission-1

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-1.py
@@ -0,0 +1,15 @@
+from collections import defaultdict
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        nums.sort()
+        occur = defaultdict(int)
+        for i in nums:
+            occur[i] += 1
+
+        sort_occur = dict(sorted(occur.items(), key=lambda item: item[1], reverse=True))
+
+        print(sorted(occur.items(), key=lambda item: item[1], reverse=True))
+
+        return list(sort_occur.keys())[0:k]
+
