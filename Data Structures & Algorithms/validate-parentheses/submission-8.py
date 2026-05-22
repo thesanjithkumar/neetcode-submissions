3c365c9df5fc63ead3930e7eb239d2aed3df8817
Sanjith Kumar <53137451+thesanjithkumar@users.noreply.github.com>
2026-05-22 14:18:30 +0530

    Add: validate-parentheses - submission-8

---
+++ Data Structures & Algorithms/validate-parentheses/submission-8.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        braces = {')': '(', '}': '{', ']': '['}
+        stack = []
+        for i in s:
+
+            if i in braces:
+                top_e = stack.pop() if stack else "#"
+
+                if top_e != braces[i]:
+                    return False
+
+            else:
+                stack.append(i)
+
+        return not stack
