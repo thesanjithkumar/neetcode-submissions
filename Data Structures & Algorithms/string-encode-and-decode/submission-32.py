1fadece20fabe7c516dc915222777e4a789d275a
Sanjith Kumar <53137451+thesanjithkumar@users.noreply.github.com>
2026-08-26 17:04:53 +0530

    Add: string-encode-and-decode - submission-32

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-32.py
@@ -0,0 +1,24 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        lens = ["/"+str(len(x)) for x in strs]
+        strs = strs + ["~"] + lens
+        print("".join(strs))
+        return "".join(strs)
+            
+
+    def decode(self, s: str) -> List[str]:
+        strs = []
+        splits = s.split('~')
+        print(splits)
+        print(splits[1].split("/")[1:])
+        lens = [int(x) for x in splits[1].split("/")[1:]]
+        current_sum = 0
+        res = [0]
+        for i in lens:
+            current_sum += i
+            res.append(current_sum)
+        for i in range(0, len(res)-1, 1):
+            # if i != len(res):
+            strs.append(s[res[i]:res[i+1]])
+        return strs
